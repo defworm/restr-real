e41956c77340c06873a3fac5ee3b7f93b40a0783
Crystal Korth <crystalkorth@gmail.com>
2022-08-08 22:31:46 -0500

    part 9 added validation errors. Possible routing issue and default image not working

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,53 +2,70 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    return (
-        <Def>
-          <main>
-            <div>
-            <h1>{ data.place.name }</h1>
-            </div>
-            
-          </main>
-          <body>
-            <div>
-            <h2>{data.place.cuisines}</h2>
-            </div>
-            <div>
-            <em>{[data.place.city,', ', data.place.state]}</em>
-            </div>
-            <br></br>
-            <div>
-                <image>{data.place.pic}</image>
-            </div>
-            <br></br>
-            <div>
-                <h2>Ratings</h2>
-                <p>Currently Unrated</p>
-            </div>
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+        <div>
+          <h3>Rating</h3>
+          <p>Not Rated</p>
+        <div className="row">
+          <div className="col-sm-6">
+            <img
+              className="col-sm-6"
+              src={data.place.pic}
+              alt={data.place.name}
+            />
+            <h3>
+              Located in {data.place.city}, {data.place.state}
+            </h3>
+          </div>
+          <div className="col-sm-6">
+            <h1>{data.place.name}</h1>
+            <h2>Rating</h2>
+            <br />
+            <h2>Description</h2>
+            <h3>{data.place.showEstablished()}</h3>
+            <h4>Serving {data.place.cuisines}</h4>
+            <br />
+            <a
+              href={`/places/${data.place.id}/edit`}
+              className="btn btn-warning"
+            >
+              Edit
+            </a>
+            {` `}
+            <form
+              method="POST"
+              action={`/places/${data.place.id}?_method=DELETE`}
+            >
+              <button type="submit" className="btn btn-danger">
+                Delete
+              </button>
+            </form>
+          </div>
+        </div>
+        <div>
+          <h3>Description</h3>
+          <p> </p>
+        </div>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
 
-            <span>
-                <h4>Comments</h4>
-                <p>No Comments Yet</p>
-            </span>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+        <hr />
+            <h2>Comments</h2>
+            <div className="row">
 
-           
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-  Edit
-</a>     
-
-<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-  <button type="submit" className="btn btn-danger">
-    Delete
-  </button>
-</form> 
-    
-    
-
-
-            </body>
-        </Def>
-    )
+            </div>
+            </div>
+      </main>
+    </Def>
+  );
 }
-
-module.exports = show
+module.exports = show;
